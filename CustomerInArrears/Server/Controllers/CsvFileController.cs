981e9c1b2d2c8f5fcee6cc1ea4bcf85094f0fd32
MDKamrulHaque <kamrulhaque@live.com>
2023-03-26 03:08:51 +0100

    Add project files.

---
+++ CustomerInArrears/Server/Controllers/CsvFileController.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using CustomerInArrears.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace CustomerInArrears.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvFileController : ControllerBase
+    {
+
+        [HttpGet("getcustomerinfo")]
+        public IEnumerable<CustomerData> GetData()
+        {
+            var streamReader = new StreamReader(@"C:\Users\Kamrul Haque\Desktop\Small Application ExampleData.csv");
+
+            var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+            var customerData = csvReader.GetRecords<CustomerData>().ToList();
+
+            return customerData;
+        }
+
+
+        [HttpPost("uploadcsvfile")]
+        public async Task<IEnumerable<CustomerData>> UploadCsvFile(IFormFile file)
+        {
+            var streamReader = new StreamReader(file.OpenReadStream());
+            var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+            var customerData = csvReader.GetRecords<CustomerData>().ToList();
+
+            return customerData;
+        }
+
+
+    }
+}
---
+++ CustomerInArrears/Shared/CustomerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerInArrears.Shared
+{
+    public class CustomerData
+    {
+        public int TenancyNumber { get; set; }
+        public string PropertyId { get; set; }
+        public string ClientName { get; set; }
+        public string MobileNumber { get; set; }
+        public decimal TenancyBalance { get; set; }
+    }
+}
